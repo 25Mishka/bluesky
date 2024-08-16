07c4c144431d6c2495fb37e12a8cb4927b027780
25Mishka <86193316+25Mishka@users.noreply.github.com>
2024-08-16 20:15:47 +0200

    search bar (city) added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("are you ?");
+function handleSearchSubmit(event) {
+  event.preventDeFault();
+  let searchIput = document.querySelector("#search-bar-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-bar-input");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
