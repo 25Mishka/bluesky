4250db1ace8a51527ee3cf198aa79332ed3cde4a
25Mishka <86193316+25Mishka@users.noreply.github.com>
2025-01-29 15:36:39 +0200

    removed and added desriptions

--- src/index.js
+++ src/index.js
@@ -5,23 +5,19 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
-
-  // Access temperature and city name based on the API response structure
-  let temperatures = response.data.temperatures.current;
-
-  console.log(response.data);
+  let icon = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML =
-    "${datee.getDay()} ${data.getHours()}:${date.getMinutes()}";
+  timeElement.innerHTML = formatDate(new Date(response.data.time * 1000));
   descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = "${response.data.condition.humidity}%";
-  temperaturesElement.innerHTML = Math.round(temperatures);
-  windSpeedElement.innerHTML = "${response.data.wind.speed}km/h";
+  humidityElement.innerHTML = `${response.data.condition.humidity}%`;
+  temperaturesElement.innerHTML = Math.round(response.data.temperature.current);
+
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" alt="Weather Icon" class="degree-icon"/>`;
 }
 
-function formateDate(date) {
+function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days = [
@@ -37,15 +33,22 @@ function formateDate(date) {
   let day = days[date.getDay()];
 
   if (minutes < 10) {
-    minutes = "0$(minutes}";
+    minutes = `0${minutes}`;
   }
 
-  return "${day}  ${hours} :${minutes}";
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
   let apiKey = "607f2b3e7boc10f3aa38ta0b3f655e47";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios
+    .get(apiUrl)
+    .then(refreshWeather)
+    .catch((error) => {
+      console.error("API Error:", error);
+    });
 }
 
 function handleSearchSubmit(event) {
@@ -64,5 +67,5 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-bar");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-// Perform a default search
+// Default city search
 searchCity("Paris");
